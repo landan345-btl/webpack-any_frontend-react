174ebc9496cd4f7f038e05e05afeb2d0abeb0377
landan345 <landan345.btl@gmail.com>
2019-06-24 15:46:28 +0800

    2nd

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,36 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '/dist'),
-    filename: 'bundle.min.js'
+    filename: 'bundle.[hash].js'
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    host: '0.0.0.0',
+    port: 3001,
+    inline: true,
+    hot: true,
+    watchOptions: {
+      ignored: ['node_modules', ],
+      aggregateTimeout: 300,
+      poll: 1500,
+    },
+  },
+  devtool: "source-map",
+
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         loader: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // 将 JS 字符串生成为 style 节点
+            "css-loader", // 将 CSS 转化成 CommonJS 模块
+            "sass-loader" // 将 Sass 编译成 CSS，默认使用 Node Sass
+        ]
       }
     ]
   },
